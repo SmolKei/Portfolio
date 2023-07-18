1ac28953b71fe3876365c0dd7ec8061d8045e1c5
Sm0Kei <kevinjm.philippe@gmail.com>
2023-07-18 15:59:10 +0200

    Added language swap & corrected menu

--- src/script/burger.js
+++ src/script/burger.js
@@ -1,71 +1,79 @@
 "use strict";
 
-const burger  = document.querySelector('.burgerbox');
+const burger = document.querySelector('.burgerbox');
 const cross = document.querySelector('.burger');
 const menu = document.querySelector('ul');
 const navbar = document.querySelector('.navbar');
-const menu1  = document.querySelectorAll('li:not(.colormod)');
-const cv  = document.querySelector('.cv');
-const menubtn  = document.querySelectorAll('li:not(.contactbtn) > a');
-burger.addEventListener("click", function() {
-    navbar.classList.toggle('open');
-    document.body.classList.toggle('open');
-    burger.animate({opacity: [0, 1]}, {duration: 2700, fill: 'forwards'});
-    if(!this.classList.contains("cross")){
-        this.classList.add("cross");
-    }
-    else
-    {
-        burger.style.left = "";
-        burger.style.transform = "";
-        menu1.forEach(item => {
-            item.style.width = "";
-        })
-        
-        menu.style.width = "";
-    }
-    
+const menu1 = document.querySelectorAll('li:not(.colormod)');
+const cv = document.querySelector('.cv');
+const menubtn = document.querySelectorAll('li:not(.contactbtn) > a');
+
+let isAnimating = false; // Flag variable to track animation state
+
+burger.addEventListener("click", function () {
+  if (isAnimating) {
+    return; // Ignore click if animation is in progress
+  }
+
+  isAnimating = true; // Set animation state to true
+
+  navbar.classList.toggle('open');
+  document.body.classList.toggle('open');
+burger.animate({ opacity: [0, 0, 1] }, { duration: 2000, fill: 'forwards' });
+
+  if (!this.classList.contains("cross")) {
+    this.classList.add("cross");
+  } else {
+    burger.style.left = "";
+    burger.style.transform = "";
+    menu1.forEach(item => {
+      item.style.width = "";
+    });
+
+    menu.style.width = "";
+  }
 });
-cross.addEventListener("transitionend", function(e) {
-    if(e.propertyName !== "transform")return;
-    if(burger.classList.contains("cross"))
-    {
-        burger.style.transform = "rotate(720deg)";
-        burger.style.left = "1.5rem";
-        menu1.forEach(item => {
-            item.style.width = "calc(90vw - 1rem)";
-        })
-        menu.style.width = "100%";
-    }
+
+cross.addEventListener("transitionend", function (e) {
+  if (e.propertyName !== "transform") return;
+  if (burger.classList.contains("cross")) {
+    burger.style.transform = "rotate(720deg)";
+    burger.style.left = "1.5rem";
+    menu1.forEach(item => {
+      item.style.width = "calc(90vw - 1rem)";
+    });
+    menu.style.width = "100%";
+  }
 });
-burger.addEventListener("transitionend", function(e) {
-    if(e.propertyName !== "left")return;
-    if(this.style.left === "")
-    {
-        this.classList.remove("cross");
-    }
-})
+
+burger.addEventListener("transitionend", function (e) {
+  if (e.propertyName !== "left") return;
+  if (this.style.left === "") {
+    this.classList.remove("cross");
+  }
+  isAnimating = false; // Reset animation state to false
+});
 
 cv.addEventListener("click", () => {
-    window.open('./public/assets/image/dots-top-menu-mobile.png').onload;
-})
+  window.open('./public/assets/image/cv1.pdf');
+});
 
 menubtn.forEach(button => {
-    button.addEventListener('click', ()=> {
-        navbar.classList.remove('open');
-        menu.style.width = "";
-        menu1.forEach(item => {
-            item.style.width = "";
-        })
-        document.body.classList.remove('open');
-        burger.style.left = "";
-        burger.style.transform = "";
-        burger.addEventListener("transitionend", function(e) {
-            if(e.propertyName !== "left")return;
-            if(this.style.left === "")
-            {
-                this.classList.remove("cross");
-            }
-        })
-    })
-})
+  button.addEventListener('click', () => {
+    if (isAnimating) {
+      return; // Ignore click if animation is in progress
+    }
+
+    isAnimating = true; // Set animation state to true
+
+    navbar.classList.remove('open');
+    menu.style.width = "";
+    menu1.forEach(item => {
+      item.style.width = "";
+    });
+    document.body.classList.remove('open');
+    burger.style.left = "";
+    burger.style.transform = "";
+    burger.animate({ opacity: [0, 0, 1] }, { duration: 2000, fill: 'forwards' });
+  });
+});
--- src/script/theme.js
+++ src/script/theme.js
@@ -7,6 +7,7 @@ const brand  = document.querySelector('.brand > h3 > a');
 const projet  = document.querySelectorAll('.projet-description');
 const mainmenu  = document.querySelector('.main-menu');
 const submenu  = document.querySelector('.sublang');
+const langmenu  = document.querySelector('.lang');
 
 function setytheme() {
    
@@ -80,16 +81,101 @@ window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", (e
 // -------------------------- choix de langue --------------------------
 
 const lang = {
-    "en" : {
-        intro1: "I'm a developpeur junior",
-        intro2: "Currently in a DWWP course, i wish to increase my skills in the dev Web and Web Mobile industry"
+    en: {
+      intro1: `I'm a junior <br><span id="swap"></span> <span class="bl">|</span>developper.`,
+      intro2: "Currently in a DWWM course, I wish to increase my skills in the Web and Mobile Web development industry.",
+      cv: "Download cv",
+      contact: "contact me",
+      title1: "Home",
+      title2: "Projects",
+      title3: "Skills",
+      button: "see",
+      next: "next",
+      prev: "previous"
     }
-}
+  };
+  
+  // Function to set the language in localStorage
+  function setLanguage(language) {
+    localStorage.setItem('language', language);
+  }
+  
+  // Function to get the language from localStorage
+  function getLanguage() {
+    return localStorage.getItem('language');
+  }
+  
+  // Function to update the language display on the page
+  function updateLanguageDisplay() {
+    const languageDisplay = document.getElementById('language-display');
+    const language = getLanguage();
+    if (language === 'en') {
+      languageDisplay.textContent = 'EN';
+      updateContent(lang[language]);
+      document.documentElement.setAttribute('lang', 'en');
+    } else {
+      languageDisplay.textContent = 'FR';
+      updateContent(null); // Revert to original HTML content
+      document.documentElement.setAttribute('lang', 'fr');
+    }
+  }
+  
+  // Function to update the HTML content based on the selected language
+  function updateContent(languageData) {
+    const introElement = document.querySelector('.intro');
+    const introElement2 = document.querySelector('.intro2');
+    const langSpan = document.querySelector('.lang span');
+    const li1  = document.querySelector('.main-menu > li:nth-child(1) > a');
+    const li2  = document.querySelector('.main-menu > li:nth-child(2) > a');
+    const li3  = document.querySelector('.main-menu > li:nth-child(3) > a');
+    const cvbtn  = document.querySelector('.cv > p');
+    const ctcbtn  = document.querySelector('.contact > p');
+    const projettitle  = document.querySelector('.project-title > h2');
+    const skilltitle  = document.querySelector('.skill-title > h2');
+    const nextbtn  = document.querySelector('.swiper-button-next > p');
+    const prevbtn  = document.querySelector('.swiper-button-prev > p');
+    const voir  = document.querySelectorAll('.voir > p > a');
 
-const sublang  = document.querySelectorAll('.sublang > li');
-
-sublang.forEach(language => {
-    language.addEventListener('click', () => {
-        
-    })
-})
+    if (languageData) {
+      introElement.innerHTML = `${languageData.intro1}`;
+      introElement2.innerHTML = `${languageData.intro2}`;
+      langSpan.textContent = document.documentElement.lang;
+      li1.innerHTML = `${languageData.title1}`;
+      li2.innerHTML = `${languageData.title2}`;
+      li3.innerHTML = `${languageData.title3}`;
+      cvbtn.innerHTML = `${languageData.cv}`;
+      ctcbtn.innerHTML = `${languageData.contact}`;
+      projettitle.innerHTML = `${languageData.title2}`;
+      skilltitle.innerHTML = `${languageData.title3}`;
+      nextbtn.innerHTML = `${languageData.next}`;
+      prevbtn.innerHTML = `${languageData.prev}`;
+      voir.forEach(see => {
+        see.innerHTML = `${languageData.button}`;
+      });
+    } 
+  }
+  
+  // Function to handle the language change event
+  function handleLanguageChange(event) {
+    event.preventDefault();
+    const selectedLanguage = event.target.getAttribute('href').substring(1);
+    setLanguage(selectedLanguage);
+    updateLanguageDisplay();
+    // Additional code to update other content based on language change
+  
+    // Reload the page to apply the language changes
+    location.reload();
+  }
+  
+  // Event listener for language links
+  const languageLinks = document.querySelectorAll('a[href="#en"], a[href="#fr"]');
+  languageLinks.forEach(link => {
+    link.addEventListener('click', handleLanguageChange);
+  });
+  
+  // On page load, update the language display
+  updateLanguageDisplay();
+  const storedLanguage = getLanguage();
+  if (storedLanguage) {
+    updateContent(lang[storedLanguage]);
+  }
